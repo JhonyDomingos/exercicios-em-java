9e228ef92f5cd3024436d22bc7a1d028d8a391ec
JhonyDomingos <jhonatandomingossilva@outlook.com.br>
2024-02-29 17:03:58 -0300

    Exercicio Fernanda Aula3

---
+++ Aula03/src/CalculoINSS.java
@@ -0,0 +1,12 @@
+public class CalculoINSS {
+    double percent = 0.15, desconto, bruto, liquido;
+
+    public void inss () {
+        desconto = bruto * percent;
+        liquido = bruto - desconto;
+
+        System.out.println("Salário  Bruto é de : " + bruto + " reais");
+        System.out.println("Dedução do INSS é de : " + desconto + " reais");
+        System.out.println("Salário Líquido é de : " + liquido + " reais");
+    }
+}
